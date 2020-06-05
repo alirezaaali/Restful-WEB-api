a89fbca8dacbc0fc7ba9456991eab72bed325008
Alireza <alireza.aali@gmail.com>
2020-06-05 14:15:53 +0430

    Add project files.

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using ContactManager.Models;
+using ContactManager.Services;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+	public class ContactController : ApiController
+	{
+		private ContactRepository contactRepository;
+		public ContactController()
+		{
+			this.contactRepository = new ContactRepository();
+		}
+
+		public Contact[] Get()
+		{
+			return contactRepository.GetAllContacts();
+		}
+	}
+
+}
